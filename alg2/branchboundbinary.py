cb9abb6affc6efe5a9ff08c1b566677f2759820c
Joao Lisboa <90469235+joaovclisboa@users.noreply.github.com>
2026-06-13 14:10:35 -0300

    arrumei a implementacao do branchboundbinary

--- alg2/branchboundbinary.py
+++ alg2/branchboundbinary.py
@@ -1,24 +1,22 @@
 class Solution:
     def Mochila_bb(self, mochila: list[list[int]], espaco: int):
-        melhor_densidade = len(mochila) * [-1]
-        melhor_densidade[len(mochila)-1] = mochila[len(mochila)-1][1]/mochila[len(mochila)-1][0]
-        for i in range(len(mochila)-2, -1, -1):
-            if (mochila[i][1]/mochila[i][0] > melhor_densidade[i+1]):
-                melhor_densidade[i] = mochila[i][1]/mochila[i][0]
-            else:
-                melhor_densidade[i] = melhor_densidade[i+1]
-
+        mochila.sort(key= lambda p:((p[1]/p[0]) if p[0] != 0 else float('inf')), reverse=True)
         melhor_solucao = -1
 
         def mochila_rec(item_at, valor_at, espaco_at):
+                nonlocal melhor_solucao
                 if item_at == len(mochila):
                     if valor_at > melhor_solucao:
                         melhor_solucao = valor_at
                     return
-                if (espaco_at * melhor_densidade[item_at] + valor_at) > melhor_solucao:
-                    print(espaco_at)
-                    if mochila[item_at][0] <= espaco_at:
-                        mochila_rec(item_at+1, valor_at + mochila[item_at][1], espaco_at-mochila[item_at][0])
+                peso_atual_item, valor_atual_item = mochila[item_at]
+                if (peso_atual_item == 0):
+                     densidade_atual = float('inf')
+                else:
+                     densidade_atual = valor_atual_item/peso_atual_item
+                if (espaco_at * densidade_atual + valor_at) > melhor_solucao:
+                    if peso_atual_item <= espaco_at:
+                        mochila_rec(item_at+1, valor_at + valor_atual_item, espaco_at-peso_atual_item)
                     mochila_rec(item_at+1, valor_at, espaco_at)
         mochila_rec(0, 0, espaco)
         print(melhor_solucao)
